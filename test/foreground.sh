24430b9bb193079859962c57b9ac8524d677f58d
Vancheeswaran Vaidyanathan <vigneshvanchy@gmail.com>
2022-08-24 10:12:30 +0530

    Update foreground.sh

--- test/foreground.sh
+++ test/foreground.sh
@@ -1,2 +1,2 @@
 echo "Creating Files"
-echo $ "print(\"Hello\")\nprint(\"How are you\")\nfor i in range(10):\n   print(i)" > test.py  
+printf "print(\"Hello\")\nprint(\"How are you\")\nfor i in range(10):\n   print(i)" > test.py  
