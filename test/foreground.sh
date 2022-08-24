3d1546cd1fb6946b466af2c08d338b87b516b2d8
Vancheeswaran Vaidyanathan <vigneshvanchy@gmail.com>
2022-08-24 10:23:02 +0530

    Update foreground.sh

--- test/foreground.sh
+++ test/foreground.sh
@@ -1,2 +1,2 @@
 echo "Creating Files"
-printf "print(\"Hello\")\nprint(\"How are you\")\nfor i in range(10):\n   print(i)" > test.py  
+printf "from flask import Flask, render_template\napp = Flask(__name__)\n@app.route(\'/\')\ndef hello_world():\n   color = {\'blue\': \'002aff\',\'yellow\': \'e1eb34\',\'green\': \'28fc03\',\'red\': \'fc1703\',\'purple\': \'b503fc\'}\n   return render_template(\'index.html\', color=color)" > test.py  
