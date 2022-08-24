9d8266d58f9b5a0ffda76a2abffe407df64b2d55
Vancheeswaran Vaidyanathan <vigneshvanchy@gmail.com>
2022-08-24 11:21:48 +0530

    Update foreground.sh

--- test/foreground.sh
+++ test/foreground.sh
@@ -1,2 +1,2 @@
 echo "Creating Files"
-printf "from flask import Flask, render_template\napp = Flask(__name__)\n@app.route(\'/\')\ndef hello_world():\n   color = {\'blue\': \'002aff\',\'yellow\': \'e1eb34\',\'green\': \'28fc03\',\'red\': \'fc1703\',\'purple\': \'b503fc\'}\n   return render_template(\'index.html\', color=color)" > test.py  
+git clone https://github.com/docker/getting-started.git
